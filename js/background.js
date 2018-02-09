8e2c3ef72f65ad873a93faf08b2fca1d8f75eebf
Adriano Chambel <acmlima.softweb@gmail.com>
2018-02-09 01:14:14 +0000

    Add key down/up for navigate in results.

--- js/background.js
+++ js/background.js
@@ -3,3 +3,12 @@ chrome.commands.onCommand.addListener(function (command) {
     window.open("../popup.html", "extension_popup", "width=800,height=600,status=no,scrollbars=yes,resizable=no");
   }
 });
+
+chrome.tabs.onUpdated.addListener(tabsBadgeCounter);
+chrome.tabs.onRemoved.addListener(tabsBadgeCounter);
+
+function tabsBadgeCounter () {
+  chrome.tabs.query({}, tabs => {
+    chrome.browserAction.setBadgeText({ text: tabs.length + '' });
+  })
+}
--- js/popup.js
+++ js/popup.js
@@ -15,8 +15,9 @@ function renderResults (tabsFound) {
                 </h5>
               </div>`;
   if (tabsFound.length) {
-    list = tabsFound.map(t => {
+    list = tabsFound.map((t, index) => {
       return `<a href="#!" class="collection-item avatar tab"
+                  tabindex="${index}"
                   data-tab-window-id="${t.windowId}"
                   data-tab-id="${t.id}">
                 <img src="${t.favIconUrl || ''}" class="circle">
@@ -39,8 +40,61 @@ function renderResults (tabsFound) {
   });
 }
 
-var query = document.querySelector('#query');
-query.addEventListener('input', function (e) {
+function selectTabItem (event, itemSelected, itemsTab) {
+  switch (event.key) {
+    case 'ArrowDown':
+      nextTabItem(itemSelected, itemsTab);
+      break;
+    case 'ArrowUp':
+      previousTabItem(itemSelected, itemsTab);
+      break;
+  }
+}
+
+function previousTabItem (itemSelected, itemsTab){
+  if (itemSelected) {
+    const previous = itemSelected.tabIndex === 0 ? itemsTab.length - 1 : itemSelected.tabIndex - 1;
+    itemsTab[previous].focus();
+  }
+  else if (itemsTab.length) {
+    itemsTab[itemsTab.length - 1].focus();
+  }
+}
+
+function nextTabItem (itemSelected, itemsTab) {
+  if (itemSelected) {
+    const next = itemSelected.tabIndex === itemsTab.length - 1 ? 0 : itemSelected.tabIndex + 1;
+    itemsTab[next].focus();
+  }
+  else if (itemsTab.length) {
+    itemsTab[0].focus();
+  }
+}
+
+function selectFirstTabItem (itemsTab) {
+  if (itemsTab.length) {
+    itemsTab[0].focus();
+  }
+}
+
+document.querySelector('#query-form').addEventListener('submit', e => {
   e.preventDefault();
-  chrome.tabs.query({},  searchInTabs);
+  const itemsTab = document.querySelectorAll('.tab');
+  if (itemsTab.length) {
+    itemsTab[0].focus();
+  }
+})
+
+document.querySelector('#query').addEventListener('input', e => {
+  e.preventDefault();
+  chrome.tabs.query({}, searchInTabs);
+});
+
+document.addEventListener('keydown', e => {
+  const eventsAllowed = ['ArrowDown', 'ArrowUp'];
+  if (eventsAllowed.includes(e.key)) {
+    const itemSelected = document.querySelector('.tab:focus');
+    const itemsTab = document.querySelectorAll('.tab');
+    selectTabItem(e, itemSelected, itemsTab);
+  }
 });
